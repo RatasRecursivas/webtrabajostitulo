37b8dc6accf3f77ef79b2297eeabd997134cf337
Patricio Pérez <patricio.perez@ceinf.cl>
2013-12-26 11:37:42 -0300

    Empezar desde cero

--- app/application/views/tesis/error.php
+++
@@ -1,7 +0,0 @@
-	<div class="row">
-		<div class="large-12 columns">
-			<div data-alert class="alert-box warning round">
-				No se encontró una tesis con tal id <a href="#" class="close">&times;</a>
-			</div>
-		</div>
-	</div>
