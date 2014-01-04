4e01ecbaf9e01f233ef17730fb9fd70a5833af44
Natalia Tarifeño <natalia.tarifeno@ceinf.cl>
2014-01-04 13:33:18 -0300

    Validaciones

--- app/application/config/form_validation.php
+++ app/application/config/form_validation.php
@@ -14,4 +14,56 @@ $config = array(
             'rules' => 'required|xss_clean|max_length[50]|trim'
         ),
     ),
-);
+    'carrera/formulario' => array(
+        array(
+            'field' => 'nombre_carrera',
+            'label' => 'Nombre Carrera',
+            'rules' => 'required|xss_clean|trim|max_length[50]|'
+        ),
+        array(
+            'field' => 'codigo',
+            'label' => 'Codigo',
+            'rules' =>'required|xss_clean|trim|max_lenght[5]|interger|'
+        )
+    ),
+    'categoria/formulario' => array(
+        array(
+            'field' => 'nombre_categoria',
+            'label' => 'Nombre Categoria',
+            'rules' => 'required|xss_clean|trim|max_length[50]|'
+        ),
+  ),
+     'tesis/formulario'=> array(
+       array(
+           'field'=>'titulo',
+           'label'=> 'titulo tesis',
+           'rules'=>'requerid|xss_clean|trim|max_length[200]|'
+       ), 
+       array(
+           'field'=>'rut',
+           'label'=> 'rut autor',
+           'rules'=>'requerid|xss_clean|trim|max_length[9]|interger|value_rut'
+     ),
+       array(
+           'field'=>'abstract',
+           'label'=> 'abstract',
+           'rules'=>'requerid|xss_clean|trim|max_length[1000]'
+           ),
+         array(
+           'field'=>'fecha_publicacion',
+           'label'=> 'fecha de publicacion',
+           'rules'=>'requerid|xss_clean|trim|value_fecha'
+             ),
+          array(
+           'field'=>'fecha_evaluacion',
+           'label'=> 'fecha de evaluacion',
+           'rules'=>'requerid|xss_clean|trim|value_fecha'
+             ),
+          array(
+           'field'=>'fecha_disponibilidad',
+           'label'=> 'fecha de disponibilidad',
+           'rules'=>'requerid|xss_clean|trim|value_fecha'
+             ),
+         
+),
+    );
